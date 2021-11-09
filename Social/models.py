1baa44e96521a6e4b21fab03797afa23cf319ae4
Julian Gomez <juliandr544@gmail.com>
2021-11-08 21:31:50 -0500

    Static files configurated

--- Social/models.py
+++ Social/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+from SocialApp.settings import TIME_ZONE
+from django.contrib.auth.models import User
+from django.utils import timezone
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE) #USER + user default my django + delete 
+    image = models.ImageField(default='batman.png')
+    def __str__(self) -> str:
+        return f'Perfil de {self.user.username}'
+
+
+class Post(models.Model):
+    user = models.ForeignKey(User , on_delete=models.CASCADE, related_name='posts')
+    timestamp = models.DateTimeField(default=timezone.now)
+    content = models.TextField()
+
+    class Meta:
+        ordering = ['-timestamp']
+    def __str__(self) -> str:
+        return f'{self.user.username} : {self.content}'
--- Social/views.py
+++ Social/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
-
+from .models import *
 def feed(request):
-    return render(request,'Social/feed.html')
+    posts = Post.objects.all()
+    ctx = {'posts': posts}
+    return render(request,'Social/feed.html', ctx)
 
 def profile(request):
     return render(request,'Social/profile.html')
