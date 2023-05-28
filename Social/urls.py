b79f4c50271269b0b7db359dd5b3a47f306892f1
Julian <40327956+Archi657@users.noreply.github.com>
2023-05-28 18:17:23 -0500

    Login/Register added
    Created login and logout, and register functionality.

--- Social/urls.py
+++ Social/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
-from . import views
+from . import views 
+from django.contrib.auth.views import LoginView, LogoutView
 urlpatterns = [
     path('', views.feed, name='feed'),
-    path('', views.profile, name='profile'),
+    path('profile/', views.profile, name='profile'),
+    path('register/', views.register, name='register'),
+    path('login/', LoginView.as_view(template_name='Social/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(template_name='Social/logout.html'), name='logout'),
 ]
--- Social/views.py
+++ Social/views.py
@@ -1,9 +1,24 @@
 from django.shortcuts import render
 from .models import *
+from .forms import UserRegisterForm
+from django.contrib import messages
+
 def feed(request):
     posts = Post.objects.all()
     ctx = {'posts': posts}
     return render(request,'Social/feed.html', ctx)
+
+def register(request):
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data['username']
+            messages.success(request, f'User {username} has been created')
+    else:
+        form = UserRegisterForm()
+    ctx = { 'form' : form }
+    return render(request, 'Social/register.html', ctx)
 
 def profile(request):
     return render(request,'Social/profile.html')
