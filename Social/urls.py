58459f707755120a0240a875dc5debf78fa727f0
Julian <40327956+Archi657@users.noreply.github.com>
2023-06-05 21:02:41 -0500

    +Create post interface

--- Social/urls.py
+++ Social/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('register/', views.register, name='register'),
     path('login/', LoginView.as_view(template_name='Social/login.html'), name='login'),
     path('logout/', LogoutView.as_view(template_name='Social/logout.html'), name='logout'),
+    path('post/', views.post, name="post"),
 ]
--- Social/views.py
+++ Social/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import *
-from .forms import UserRegisterForm
+from .forms import UserRegisterForm, PostForm
 from django.contrib import messages
 
 def feed(request):
@@ -19,6 +19,21 @@ def register(request):
         form = UserRegisterForm()
     ctx = { 'form' : form }
     return render(request, 'Social/register.html', ctx)
+
+def post(request):
+    current_user = get_object_or_404(User, pk=request.user.pk)
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.user = current_user
+            post.save()
+            messages.success(request, 'Post enviado')
+            return redirect('feed')
+    else:
+        form = PostForm()
+    return render(request, 'social/post.html', {'form':form})
+
 
 def profile(request):
     return render(request,'Social/profile.html')
