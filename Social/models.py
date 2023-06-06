7948e140f5cb4659eee7e26067b64d143bf9d91f
Julian <40327956+Archi657@users.noreply.github.com>
2023-06-05 20:43:15 -0500

    Auto-create profile
    auto-create profile when an user is registered

--- Social/models.py
+++ Social/models.py
@@ -4,12 +4,13 @@ from SocialApp.settings import TIME_ZONE
 from django.contrib.auth.models import User
 from django.utils import timezone
 
+from django.db.models.signals import post_save
+
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE) #USER + user default my django + delete 
     image = models.ImageField(default='batman.png')
     def __str__(self) -> str:
         return f'Perfil de {self.user.username}'
-
 
 class Post(models.Model):
     user = models.ForeignKey(User , on_delete=models.CASCADE, related_name='posts')
@@ -20,3 +21,8 @@ class Post(models.Model):
         ordering = ['-timestamp']
     def __str__(self) -> str:
         return f'{self.user.username} : {self.content}'
+
+def create_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+post_save.connect(create_profile, sender=User)
